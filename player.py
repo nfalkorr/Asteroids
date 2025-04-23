17bd3017231b71a566c62d9a64c404133a8650cb
user <nfasan@nfalkorr.org>
2025-04-23 14:46:51 -0700

    add player logic for movement, other minor changes

--- player.py
+++ player.py
@@ -1,5 +1,4 @@
-from constants import PLAYER_RADIUS
-from constants import ROTATION_SPEED
+from constants import *
 from circleshape import CircleShape
 import pygame
 
@@ -10,7 +9,7 @@ class Player(CircleShape):
 		self.rotation = 0 # initialize player stationary
 
 	def triangle(self): #points relative to each other, draw in next method
-	    forward = pygame.Vector2(0, 1).rotate(self.rotation) # calculating highest/forwardmost point of triangle
+	    forward = pygame.Vector2(0, 1).rotate(self.rotation) # calculating forwardmost point of triangle
 	    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5 #calculate rightmost side of triangle
 	    a = self.position + forward * self.radius # point a is the center of the triangle (self.position) + the forwardmost point * the defined radius of the player passed in from CircleShape
 	    b = self.position - forward * self.radius - right #leftmost point calculation
@@ -25,9 +24,18 @@ class Player(CircleShape):
 		self.rotation = self.rotation + (ROTATION_SPEED * dt) # calculate rotation based on current + speed constant * delta value
 
 	def update(self, dt):
-	    keys = pygame.key.get_pressed()
+		keys = pygame.key.get_pressed()
 
-	    if keys[pygame.K_a]:
-	        self.rotate(-dt) #rotate left based on A - key press
-	    if keys[pygame.K_d]:
-	        self.rotate(dt) #rotate right based on D - key press
+		if keys[pygame.K_a]:
+			self.rotate(-dt) #rotate left based on A - keypress
+		if keys[pygame.K_d]:
+			self.rotate(dt) #rotate right based on D - keypress
+		if keys[pygame.K_w]:
+			self.move(dt) #move up based on W - keypress
+		if keys[pygame.K_s]:
+			self.move(-dt) #move down based on S - keypress
+
+	def move(self,dt):
+		forward = pygame.Vector2(0, 1).rotate(self.rotation) #update 
+		self.position += forward * PLAYER_SPEED * dt # move forward based on previous position modified by speed and clock update
+
